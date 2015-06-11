eb921decd5e02dd70c4685b7fb276f26e0884eb2
Iszak Bryan <iszak.bryan@gmail.com>
2015-06-11 23:06:40 +0200

    Added some good default packages

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,6 +3,7 @@ node default {
 
   create_resources(account, hiera_hash('account', {}))
   create_resources(file, hiera_hash('file', {}))
+  create_resources(package, hiera_hash('package', {}))
 
   create_resources(git::config, hiera_hash('git::config', {}))
   create_resources(postgresql::server::role, hiera_hash('postgresql::server::role', {}))
