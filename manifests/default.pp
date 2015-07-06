8c0b79755f4622b873a9989f169aad94827574ce
Iszak Bryan <iszak.bryan@gmail.com>
2015-07-07 00:05:38 +0100

    Use account over specific resource types

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,9 +2,4 @@ node default {
   hiera_include('classes')
 
   create_resources(account, hiera_hash('account', {}))
-  create_resources(file, hiera_hash('file', {}))
-  create_resources(package, hiera_hash('package', {}))
-
-  create_resources(git::config, hiera_hash('git::config', {}))
-  create_resources(postgresql::server::role, hiera_hash('postgresql::server::role', {}))
 }
